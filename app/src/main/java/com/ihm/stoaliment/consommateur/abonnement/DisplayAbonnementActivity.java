640ebf6236568d1bdf89abd576d74efd26284878
Abdel <abdelouhab.belkhiri@etu.unice.fr>
2020-05-12 16:14:34 +0200

    Quick fix

--- app/src/main/java/com/ihm/stoaliment/consommateur/abonnement/DisplayAbonnementActivity.java
+++ app/src/main/java/com/ihm/stoaliment/consommateur/abonnement/DisplayAbonnementActivity.java
@@ -2,11 +2,16 @@ package com.ihm.stoaliment.consommateur.abonnement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.ihm.stoaliment.authentification.AuthentificationActivity;
 import com.ihm.stoaliment.consommateur.BaseConsommateurActivity;
 import com.ihm.stoaliment.consommateur.accueil.AbonnementListAdapter;
 import com.ihm.stoaliment.controleur.AbonneControleur;
@@ -49,6 +54,37 @@ public class DisplayAbonnementActivity extends BaseConsommateurActivity implemen
     @Override
     public int getBottomNavigationMenuItemId() {
         return R.id.action_fav;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_item_deconnexion) {
+
+            SharedPreferences mPrefs = Authentification.preferences;
+            SharedPreferences.Editor prefsEditor = mPrefs.edit();
+            prefsEditor.remove(Authentification.GSON_LABEL);
+            prefsEditor.apply();
+
+            Intent intent = new Intent(this, AuthentificationActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
